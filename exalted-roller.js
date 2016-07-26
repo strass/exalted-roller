ee35c5e1aeb034bb5f0bc69a0caffc4a100577ab
Zak Strassberg <zakstrassberg@gmail.com>
2016-07-26 16:31:37 -0700

    improve syntax and a bit of refactoring

--- exalted-roller.js
+++ exalted-roller.js
@@ -3,34 +3,51 @@ var mybot = new Discord.Client();
 credentials = require("./token.js")
 mybot.loginWithToken(credentials.token);
 
-// var Parser = require("simple-text-parser");
-// var parser = new Parser();
-
+// Look for messages starting with roll
+// To-do: change to .roll
 mybot.on("message", function(message) {
     if (message.content.startsWith("roll")) {
         mybot.reply(message, parseMessage(message));
     }
 });
 
+//
+// SYNTAX GUIDE:
+// Handle: target number, double successes (single and #+),
+// rerolls (single and cascading), autosuccess
+//
+// .roll/tn6/
+// tn: single target number, values >= to this will count as a success. Default: 7
+// db: double x's. 7 double's 7 only, 7+ is 7 and up. Default: 10
+// re: reroll #
+// as: adds a flat number of successes
+//
+
+
 function parseMessage(message) {
-    // get # of dice. Look for int or [int]d10
-    // split at NOT int or [int]d10
     message = message.toString();
     var parsed = message.split(" ");
+
+    // log parsed message for debugging:
+    console.log(parsed);
 
     // Look for a digit after "roll"
     if (parsed[1].match(/^\d+/g)) {
         // get digits at beginning of string
+        // I'm fairly sure this could be improved upon...
         numDice = parsed[1].match(/^\d+/g);
         numDice = numDice[0];
         countSuccesses(roll(numDice), target, doubleIfGreaterThan);
         return roll(numDice);
 
     } else {
+        // Bad syntax handling
+        // To-do: add better support here
         return "I can't find any numbers after roll. Syntax: roll 8d10";
     }
 }
 
+// Dice rolling:
 function roll(n) {
     var rolls = [];
     var i = 0;
@@ -46,6 +63,7 @@ function rolld10() {
     return Math.floor(Math.random() * 10 + 1);
 }
 
+// Dice handling:
 function countSuccesses(rolls, target, doubleIfGreaterThan) {
     for (var i in rolls) {
         if (rolls[i] >= doubleIfGreaterThan) {
